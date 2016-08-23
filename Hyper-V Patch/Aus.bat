02262ed0116cd666e99daa0c01914b52c0644caa
xpsony <xpsony@web.de>
2016-08-23 16:04:46 +0200

    Hyper V Patch

---
+++ Hyper-V Patch/Aus.bat
@@ -0,0 +1,19 @@
+@Echo off
+REM Wechsele Zeichensatz um Umlaute darzustellen.
+CHCP 1252
+cls
+echo Es werden Admin Rechte benötigt. Check auf Adminrechten...
+echo.
+echo Ergebnis :
+net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Admin Rechte erkannt. Befehl wird ausgeführt ...
+	bcdedit /set hypervisorlaunchtype auto
+	Timeout 4 > nul
+	Exit
+    ) else (
+        echo Starte diese Batch als Administrator um den Patch Vorgang zu starten.
+    )
+
+    Timeout 4 > nul
+Exit
